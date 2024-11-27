79cbc827b8e4280c7c757cfe835284f464eaa9f9
irapuanjr13 <irapuanjunior13@gmail.com>
2024-11-27 08:20:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ def gerar_guia():
         pdf.add_table(dados_bmps)
         pdf.add_details(secao_destino, chefia_origem, secao_origem, chefia_destino)
         
-        output_path = "guia_circulacao.pdf"
+        output_path = "static/guia_circulacao_interna.pdf"
         pdf.output(output_path)
         print(f"PDF gerado em {output_path}")
         
