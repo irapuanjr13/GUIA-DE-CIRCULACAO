206fab05dde9ea75774215ee1fc2ca2f187b3b54
irapuanjr13 <irapuanjunior13@gmail.com>
2024-11-28 15:15:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -53,23 +53,23 @@ class PDF(FPDF):
         self.set_font("Arial", "B", 10)
         for width, header in zip(col_widths, headers):
             self.cell(width, 10, header, border=1, align="C")
-        self.ln()
+            self.ln()
 
-        # Adicionar as linhas da tabela
+    # Adicionar as linhas da tabela
         self.set_font("Arial", size=10)
         for _, row in dados_bmps.iterrows():
     # Calcular a altura necessária para a célula "Nomenclatura"
-    text = self.fix_text(row["NOMECLATURA/COMPONENTE"])
-    line_count = self.get_string_width(text) // col_widths[1] + 1
-    row_height = 10 * line_count  # 10 é a altura padrão da célula
+        text = self.fix_text(row["NOMECLATURA/COMPONENTE"])
+        line_count = self.get_string_width(text) // col_widths[1] + 1
+        row_height = 10 * line_count  # 10 é a altura padrão da célula
 
-    self.cell(col_widths[0], row_height, str(row["Nº BMP"]), border=1, align="C")
+        self.cell(col_widths[0], row_height, str(row["Nº BMP"]), border=1, align="C")
 
-    x, y = self.get_x(), self.get_y()
-    self.multi_cell(col_widths[1], 10, text, border=1)
-    self.set_xy(x + col_widths[1], y)  # Reposicionar para a próxima coluna
+        x, y = self.get_x(), self.get_y()
+        self.multi_cell(col_widths[1], 10, text, border=1)
+        self.set_xy(x + col_widths[1], y)  # Reposicionar para a próxima coluna
 
-    self.cell(col_widths[2], row_height, self.fix_text(str(row["Nº SERIE"])), border=1, align="C")
+        self.cell(col_widths[2], row_height, self.fix_text(str(row["Nº SERIE"])), border=1, align="C")
 
     # Tratar valores numéricos
     if pd.isnull(row['VL. ATUALIZ.']):
