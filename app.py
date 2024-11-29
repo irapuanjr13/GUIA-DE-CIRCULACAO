70e378f66b05de2a9d00defb8c66ff2690ee6398
irapuanjr13 <irapuanjunior13@gmail.com>
2024-11-29 16:28:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,14 +21,16 @@ def get_excel_from_google_drive():
 # Carregar a planilha no início do programa
 df = get_excel_from_google_drive()
 
-pdf = PDF()
-pdf.add_page()
-pdf.add_table(dados_bmps)
-pdf.add_details(secao_destino, chefia_origem, secao_origem, chefia_destino)
+    pdf = PDF()
+        pdf.add_page()
+        pdf.add_table(dados_bmps)
+        pdf.add_details(secao_destino, chefia_origem, secao_origem, chefia_destino)
 
-output_path = "static/guia_circulacao_interna.pdf"
-pdf.output(output_path)
-return send_file(output_path, as_attachment=True)
+        output_path = os.path.join(app.static_folder, "guia_circulacao_interna.pdf")
+        pdf.output(output_path)
+        return send_file(output_path, as_attachment=True)
+
+    return render_template("guia_bens.html", secoes_origem=secoes_origem, secoes_destino=secoes_destino)
 
 class PDF(FPDF):
     def __init__(self):
