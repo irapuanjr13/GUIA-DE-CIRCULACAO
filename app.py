fc148a751e211ec9f2e0f694c7bdcf0b1dbbbd0d
irapuanjr13 <irapuanjunior13@gmail.com>
2024-11-29 08:32:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -135,9 +135,9 @@ def guia_bens():
                 error="Itens da conta '87 - MATERIAL DE CONSUMO DE USO DURADOURO' não podem ser processados."
             )
 
-                 results = dados_bmps
+                results = dados_bmps
 
-        return render_template(
+    return render_template(
         "guia_bens.html",
         secoes_origem=secoes_origem,
         secoes_destino=secoes_destino,
