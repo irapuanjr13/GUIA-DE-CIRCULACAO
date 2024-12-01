c037fef3a8ea52030e3a5fa2269394476acd1e5d
irapuanjr13 <irapuanjunior13@gmail.com>
2024-12-01 20:46:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,88 @@ def get_excel_from_google_drive():
 # Carregar a planilha no início do programa
 df = get_excel_from_google_drive()
 
+class PDF(FPDF):
+    def __init__(self):
+        super().__init__('P', 'mm', 'A4')  # Orientação retrato, milímetros, formato A4
+
+    def header(self):
+        # Cabeçalho centralizado
+        self.set_font("Arial", "B", 12)
+        self.cell(0, 6, "MINISTÉRIO DA DEFESA", ln=True, align="C")
+        self.cell(0, 6, "COMANDO DA AERONÁUTICA", ln=True, align="C")
+        self.cell(0, 6, "GRUPAMENTO DE APOIO DE LAGOA SANTA", ln=True, align="C")
+        self.cell(0, 8, "GUIA DE MOVIMENTAÇÃO DE BEM MÓVEL PERMANENTE ENTRE AS SEÇÕES DO GAPLS", ln=True, align="C")
+        self.ln(10)
+
+    def fix_text(self, text):
+        """Corrige caracteres incompatíveis com a codificação latin-1."""
+        replacements = {
+            "–": "-",  # Substituir travessão por hífen
+            "“": '"',  # Substituir aspas abertas por aspas duplas
+            "”": '"',  # Substituir aspas fechadas por aspas duplas
+            "’": "'",  # Substituir apóstrofo por aspas simples
+        }
+        for old, new in replacements.items():
+            text = text.replace(old, new)
+        return text
+
+    def add_table(self, dados_bmps):
+        col_widths = [25, 70, 55, 35]
+        headers = ["Nº BMP", "Nomenclatura", "Nº Série", "Valor Atualizado"]
+
+        self.set_font("Arial", "B", 10)
+        for width, header in zip(col_widths, headers):
+            self.cell(width, 10, header, border=1, align="C")
+        self.ln()
+
+        self.set_font("Arial", size=10)
+        for _, row in dados_bmps.iterrows():
+            text = self.fix_text(row["NOMECLATURA/COMPONENTE"])
+            text = self.fix_text(row["NOMECLATURA/COMPONENTE"])
+            line_count = (self.get_string_width(text) // (col_widths[1] - 5)) + 1
+            row_height = line_count * 10
+
+            self.cell(col_widths[0], row_height, str(row["Nº BMP"]), border=1, align="C")
+            x, y = self.get_x(), self.get_y()
+            self.multi_cell(col_widths[1], 10, text, border=1)
+            self.set_xy(x + col_widths[1], y)
+
+            self.cell(col_widths[2], row_height, self.fix_text(str(row["Nº SERIE"])), border=1, align="C")
+            valor_atualizado = f"R$ {row['VL. ATUALIZ.']:.2f}".replace('.', ',') if not pd.isnull(row['VL. ATUALIZ.']) else "N/A"
+            self.cell(col_widths[3], row_height, valor_atualizado, border=1, align="R")
+            self.ln()
+
+    def add_details(self, secao_destino, chefia_origem, secao_origem, chefia_destino):
+        text = f"""
+Solicitação de Transferência:
+Informo à Senhora Chefe do GAP-LS que os bens especificados estão inservíveis para uso neste setor, classificados como ociosos, recuperáveis, reparados ou novos - aguardando distribuição. Diante disso, solicito autorização para transferir o(s) Bem(ns) Móvel(is) Permanente(s) acima discriminado(s), atualmente sob minha guarda, para a Seção {secao_destino}.
+
+{chefia_origem}
+{secao_origem}
+
+Confirmação da Seção de Destino:
+Estou ciente da movimentação informada acima e, devido à necessidade do setor, solicito à Senhora Dirigente Máximo autorização para manter sob minha guarda os Bens Móveis Permanentes especificados.
+
+{chefia_destino}
+{secao_destino}
+
+DO AGENTE DE CONTROLE INTERNO AO DIRIGENTE MÁXIMO
+Informo à Senhora que, após conferência, foi verificado que esta guia cumpre o disposto no Módulo D do RADA-e e, conforme a alínea "d" do item 5.3 da ICA 179-1, encaminho para apreciação e se for o caso, autorização.
+
+KARINA RAQUEL VALIMAREANU  Maj Int
+Chefe da ACI
+
+DESPACHO DA AGENTE DIRETOR
+Autorizo a movimentação solicitada e determino:
+1. Que a Seção de Registro realize a movimentação no SILOMS.
+2. Que a Seção de Registro publique a movimentação no próximo aditamento a ser confeccionado, conforme o item 2.14.2, Módulo do RADA-e.
+3. Que os detentores realizem a movimentação física do(s) bem(ns).
+
+LUCIANA DO AMARAL CORREA  Cel Int
+Dirigente Máximo
+"""
+        self.multi_cell(0, 8, self.fix_text(text))
+        
 @app.route("/")
 def menu_principal():
     return render_template("index.html")
@@ -106,88 +188,6 @@ def get_chefia():
 
     return jsonify({"chefia": chefia.tolist()})
 
-class PDF(FPDF):
-    def __init__(self):
-        super().__init__('P', 'mm', 'A4')  # Orientação retrato, milímetros, formato A4
-
-    def header(self):
-        # Cabeçalho centralizado
-        self.set_font("Arial", "B", 12)
-        self.cell(0, 6, "MINISTÉRIO DA DEFESA", ln=True, align="C")
-        self.cell(0, 6, "COMANDO DA AERONÁUTICA", ln=True, align="C")
-        self.cell(0, 6, "GRUPAMENTO DE APOIO DE LAGOA SANTA", ln=True, align="C")
-        self.cell(0, 8, "GUIA DE MOVIMENTAÇÃO DE BEM MÓVEL PERMANENTE ENTRE AS SEÇÕES DO GAPLS", ln=True, align="C")
-        self.ln(10)
-
-    def fix_text(self, text):
-        """Corrige caracteres incompatíveis com a codificação latin-1."""
-        replacements = {
-            "–": "-",  # Substituir travessão por hífen
-            "“": '"',  # Substituir aspas abertas por aspas duplas
-            "”": '"',  # Substituir aspas fechadas por aspas duplas
-            "’": "'",  # Substituir apóstrofo por aspas simples
-        }
-        for old, new in replacements.items():
-            text = text.replace(old, new)
-        return text
-
-    def add_table(self, dados_bmps):
-        col_widths = [25, 70, 55, 35]
-        headers = ["Nº BMP", "Nomenclatura", "Nº Série", "Valor Atualizado"]
-
-        self.set_font("Arial", "B", 10)
-        for width, header in zip(col_widths, headers):
-            self.cell(width, 10, header, border=1, align="C")
-        self.ln()
-
-        self.set_font("Arial", size=10)
-        for _, row in dados_bmps.iterrows():
-            text = self.fix_text(row["NOMECLATURA/COMPONENTE"])
-            text = self.fix_text(row["NOMECLATURA/COMPONENTE"])
-            line_count = (self.get_string_width(text) // (col_widths[1] - 5)) + 1
-            row_height = line_count * 10
-
-            self.cell(col_widths[0], row_height, str(row["Nº BMP"]), border=1, align="C")
-            x, y = self.get_x(), self.get_y()
-            self.multi_cell(col_widths[1], 10, text, border=1)
-            self.set_xy(x + col_widths[1], y)
-
-            self.cell(col_widths[2], row_height, self.fix_text(str(row["Nº SERIE"])), border=1, align="C")
-            valor_atualizado = f"R$ {row['VL. ATUALIZ.']:.2f}".replace('.', ',') if not pd.isnull(row['VL. ATUALIZ.']) else "N/A"
-            self.cell(col_widths[3], row_height, valor_atualizado, border=1, align="R")
-            self.ln()
-
-    def add_details(self, secao_destino, chefia_origem, secao_origem, chefia_destino):
-        text = f"""
-Solicitação de Transferência:
-Informo à Senhora Chefe do GAP-LS que os bens especificados estão inservíveis para uso neste setor, classificados como ociosos, recuperáveis, reparados ou novos - aguardando distribuição. Diante disso, solicito autorização para transferir o(s) Bem(ns) Móvel(is) Permanente(s) acima discriminado(s), atualmente sob minha guarda, para a Seção {secao_destino}.
-
-{chefia_origem}
-{secao_origem}
-
-Confirmação da Seção de Destino:
-Estou ciente da movimentação informada acima e, devido à necessidade do setor, solicito à Senhora Dirigente Máximo autorização para manter sob minha guarda os Bens Móveis Permanentes especificados.
-
-{chefia_destino}
-{secao_destino}
-
-DO AGENTE DE CONTROLE INTERNO AO DIRIGENTE MÁXIMO
-Informo à Senhora que, após conferência, foi verificado que esta guia cumpre o disposto no Módulo D do RADA-e e, conforme a alínea "d" do item 5.3 da ICA 179-1, encaminho para apreciação e se for o caso, autorização.
-
-KARINA RAQUEL VALIMAREANU  Maj Int
-Chefe da ACI
-
-DESPACHO DA AGENTE DIRETOR
-Autorizo a movimentação solicitada e determino:
-1. Que a Seção de Registro realize a movimentação no SILOMS.
-2. Que a Seção de Registro publique a movimentação no próximo aditamento a ser confeccionado, conforme o item 2.14.2, Módulo do RADA-e.
-3. Que os detentores realizem a movimentação física do(s) bem(ns).
-
-LUCIANA DO AMARAL CORREA  Cel Int
-Dirigente Máximo
-"""
-        self.multi_cell(0, 8, self.fix_text(text))
-        
 @app.route('/gerar_guia', methods=['POST'])
 def gerar_guia():
     data = request.json
