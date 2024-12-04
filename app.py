fdd3bbe6ebc70acea842fb01991076144a02b602
irapuanjr13 <irapuanjunior13@gmail.com>
2024-12-04 16:20:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,8 @@ def guia_bens():
     secao_destino = df["Seção de Destino"].dropna().unique().tolist()
 
     data = request.get_json(silent=True)
-if not data:
-    return jsonify({"error": "Os dados enviados não estão no formato JSON!"}), 400
+    if not data:
+        return jsonify({"error": "Os dados enviados não estão no formato JSON!"}), 400
         
         print("Dados recebidos:", data)
 
