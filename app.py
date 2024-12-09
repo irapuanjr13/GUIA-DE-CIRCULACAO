9339ef094d08fb62d84ec5953085f7fc5f1d3398
irapuanjr13 <irapuanjunior13@gmail.com>
2024-12-09 13:00:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,14 +32,6 @@ def guia_bens():
         secoes_destino = df["Seção de Destino"].dropna().unique().tolist()
         return render_template("guia_bens.html", secoes_origem=secoes_origem, secoes_destino=secoes_destino)
 
-    if not dados_bmps["CONTA"].eq("87 - MATERIAL DE CONSUMO DE USO DURADOURO").any():
-        return render_template(
-        "guia_bens.html",
-        secoes_origem=secoes_origem,
-        secoes_destino=secoes_destino,
-        error="Estes itens pertencem à conta '87 - MATERIAL DE CONSUMO DE USO DURADOURO'."
-    )
-
     elif request.method == "POST":
         try:
             dados = request.json
@@ -54,7 +46,15 @@ def guia_bens():
             bmp_list = [bmp.strip() for bmp in dados["bmp_numbers"]]
             dados_bmps = df[df["Nº BMP"].astype(str).isin(bmp_list)]
             if dados_bmps.empty:
-                return jsonify({"error": "Nenhum BMP válido encontrado."}), 400          
+                return jsonify({"error": "Nenhum BMP válido encontrado."}), 400
+                
+            if not dados_bmps["CONTA"].eq("87 - MATERIAL DE CONSUMO DE USO DURADOURO").any():
+                return render_template(
+                "guia_bens.html",
+                secoes_origem=secoes_origem,
+                secoes_destino=secoes_destino,
+                error="Estes itens pertencem à conta '87 - MATERIAL DE CONSUMO DE USO DURADOURO'."
+            )
 
             # Processamento do PDF ou outros passos podem ir aqui
             return jsonify({"message": "Dados processados com sucesso."})
