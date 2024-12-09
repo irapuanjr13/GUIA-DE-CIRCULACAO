4e105c4e892b7b57f6496c4e6481b19674274747
irapuanjr13 <irapuanjunior13@gmail.com>
2024-12-09 12:42:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -43,13 +43,13 @@ def guia_bens():
                 if not dados.get(campo):
                     return jsonify({"error": f"O campo '{campo}' é obrigatório."}), 400
 
-                if not dados_bmps["CONTA"].eq("87 - MATERIAL DE CONSUMO DE USO DURADOURO").any():
-                    return render_template(
-                        "guia_bens.html",
-                        secoes_origem=secoes_origem,
-                        secoes_destino=secoes_destino,
-                        error="Estes itens pertencem à conta '87 - MATERIAL DE CONSUMO DE USO DURADOURO'."
-                    )
+            if not dados_bmps["CONTA"].eq("87 - MATERIAL DE CONSUMO DE USO DURADOURO").any():
+                return render_template(
+                    "guia_bens.html",
+                    secoes_origem=secoes_origem,
+                    secoes_destino=secoes_destino,
+                    error="Estes itens pertencem à conta '87 - MATERIAL DE CONSUMO DE USO DURADOURO'."
+                )
 
             bmp_list = [bmp.strip() for bmp in dados["bmp_numbers"]]
             dados_bmps = df[df["Nº BMP"].astype(str).isin(bmp_list)]
