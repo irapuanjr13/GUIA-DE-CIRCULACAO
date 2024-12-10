4cc9fb10081e6e4c0807e43f7e795722fbe99593
irapuanjr13 <irapuanjunior13@gmail.com>
2024-12-10 14:28:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -472,7 +472,7 @@ def ttac_apontamentos_form():
         return send_file(caminho_arquivo, as_attachment=True)
     
             # Renderiza o formulário
-    return render_template('ttac_apontamentos.html')
+    return render_template('TTAC_apontamentos.html')
 
 @app.route("/consulta_bmp", methods=["GET", "POST"])
 def consulta_bmp():
